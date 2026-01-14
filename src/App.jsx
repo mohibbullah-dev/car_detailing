9945091247011ed73d760780b7eab8fa588da8fd
Md Mohibbullah <mohammedhanif74612@gmail.com>
2026-01-14 12:16:25 +0200

    modi: app.jsx modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,39 @@
-// import { Routes, Route } from "react-router-dom";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
-
-// import Home from "./pages/Home";
-// import Portfolio from "./pages/Portfolio";
-// import AdminUpload from "./pages/AdminUpload";
-
-// export default function App() {
-//   return (
-//     <div className="min-h-screen bg-white text-zinc-900">
-//       <Header />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/portfolio" element={<Portfolio />} />
-//         <Route path="/admin/upload" element={<AdminUpload />} />
-//       </Routes>
-//       <Footer />
-//     </div>
-//   );
-// }
-
 import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import AdminLogin from "./pages/AdminLogin";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import AdminUpload from "./pages/AdminUpload";
-import AdminLogin from "./pages/AdminLogin";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/portfolio" element={<Portfolio />} />
-      <Route path="/admin/login" element={<AdminLogin />} />
-      <Route path="/admin/upload" element={<AdminUpload />} />
-    </Routes>
+    <div className="min-h-screen bg-white text-zinc-900">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin/upload" element={<AdminUpload />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
+
+// import { Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+// import Portfolio from "./pages/Portfolio";
+// import AdminUpload from "./pages/AdminUpload";
+// import AdminLogin from "./pages/AdminLogin";
+
+// export default function App() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/portfolio" element={<Portfolio />} />
+//       <Route path="/admin/login" element={<AdminLogin />} />
+//       <Route path="/admin/upload" element={<AdminUpload />} />
+//     </Routes>
+//   );
+// }
