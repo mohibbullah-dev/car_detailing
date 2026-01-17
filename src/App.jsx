400e0e3dd1ab88152d79e3d0e0aac912a29016a9
Md Mohibbullah <mohammedhanif74612@gmail.com>
2026-01-17 21:20:05 +0200

    modi: app.jsx is modified

--- src/App.jsx
+++ src/App.jsx
@@ -24,20 +24,3 @@ export default function App() {
     </div>
   );
 }
-
-// import { Routes, Route } from "react-router-dom";
-// import Home from "./pages/Home";
-// import Portfolio from "./pages/Portfolio";
-// import AdminUpload from "./pages/AdminUpload";
-// import AdminLogin from "./pages/AdminLogin";
-
-// export default function App() {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Home />} />
-//       <Route path="/portfolio" element={<Portfolio />} />
-//       <Route path="/admin/login" element={<AdminLogin />} />
-//       <Route path="/admin/upload" element={<AdminUpload />} />
-//     </Routes>
-//   );
-// }
