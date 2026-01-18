98b3759efcb0f743bb5ff8ef592e35542d1c9232
Md Mohibbullah <mohammedhanif74612@gmail.com>
2026-01-18 10:42:11 +0200

    modified

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import PriceCalculator from "../components/PriceCalculator";
 import Services from "../components/Services";
 import PreviousWorkSlider from "../components/PreviousWorkSlider";
 import TestimonialSlider from "../components/TestimonialSlider";
+import MaintenanceSection from "../components/MaintenanceSection";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <PreviousWorkSlider limit={4} />
+      <MaintenanceSection />
       <PriceCalculator />
       <Services />
       <TestimonialSlider />
