783eba3c4418ec0de5e0a65810cd76f9b48345d1
Md Mohibbullah <mohammedhanif74612@gmail.com>
2026-01-22 21:01:30 +0200

    modi: AdminPortfolio is working

--- src/pages/AdminPortfolio.jsx
+++ src/pages/AdminPortfolio.jsx
@@ -613,7 +613,7 @@ export default function AdminPortfolio() {
         </div>
 
         {/* Portfolio List */}
-        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 items-start">
           {data?.map((item) => {
             const isEditing = editId === item._id;
 
